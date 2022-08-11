2942cb44356d9039860d768db80d28e6408c1118
Oleg Kirilyuk <okirilyuk@Olegs-MacBook-Pro.local>
2022-08-11 10:12:58 +0400

    add classes

--- test/test_get_pet_by_id.py
+++ test/test_get_pet_by_id.py
@@ -3,20 +3,21 @@ from utils import test_data
 from hamcrest import assert_that, is_
 
 
-def test_get_pet_by_id(data=test_data.random_pet):
-    requests.post(url="https://petstore.swagger.io/v2/pet", json=data, headers={
-        'Content-type': 'application/json',
-        'Accept': 'application/json'
-    })
-    resp = requests.get(url=f"https://petstore.swagger.io/v2/pet/{data['id']}")
-    assert_that(resp.status_code, is_(200))
-    assert_that(resp.json()['id'], is_(data['id']))
-    assert_that(resp.json()['name'], is_('doggie'))
-    assert_that(resp.json()['status'], is_('available'))
+class TestGetPetById:
 
+    def test_get_pet_by_id(self, data=test_data.random_pet):
+        requests.post(url="https://petstore.swagger.io/v2/pet", json=data, headers={
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        })
+        resp = requests.get(url=f"https://petstore.swagger.io/v2/pet/{data['id']}")
+        assert_that(resp.status_code, is_(200))
+        assert_that(resp.json()['id'], is_(data['id']))
+        assert_that(resp.json()['name'], is_('doggie'))
+        assert_that(resp.json()['status'], is_('available'))
 
-def test_get_pet_by_id_negative(pet_id=142255125):
-    resp = requests.get(url=f"https://petstore.swagger.io/v2/pet/{pet_id}")
-    assert_that(resp.status_code, is_(404))
-    assert_that(resp.json()['type'], is_('error'))
-    assert_that(resp.json()['message'], is_('Pet not found'))
+    def test_get_pet_by_id_negative(self, pet_id=142255125):
+        resp = requests.get(url=f"https://petstore.swagger.io/v2/pet/{pet_id}")
+        assert_that(resp.status_code, is_(404))
+        assert_that(resp.json()['type'], is_('error'))
+        assert_that(resp.json()['message'], is_('Pet not found'))
--- test/test_update_a_pet.py
+++ test/test_update_a_pet.py
@@ -3,15 +3,17 @@ from utils import test_data
 from hamcrest import assert_that, is_
 
 
-def test_update_a_pet(data=test_data.random_pet):
-    requests.post(url="https://petstore.swagger.io/v2/pet", json=data, headers={
-        'Content-type': 'application/json',
-        'Accept': 'application/json'
-    })
-    data['name'] = 'cat'
-    resp = requests.put(url=f"https://petstore.swagger.io/v2/pet", json=data, headers={
-        'Content-type': 'application/json',
-        'Accept': 'application/json'
-    })
-    assert_that(resp.status_code, is_(200))
-    assert_that(resp.json()['name'], is_('cat'))
+class TestUpdatePet:
+
+    def test_update_a_pet(self, data=test_data.random_pet):
+        requests.post(url="https://petstore.swagger.io/v2/pet", json=data, headers={
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        })
+        data['name'] = 'cat'
+        resp = requests.put(url=f"https://petstore.swagger.io/v2/pet", json=data, headers={
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        })
+        assert_that(resp.status_code, is_(200))
+        assert_that(resp.json()['name'], is_('cat'))
